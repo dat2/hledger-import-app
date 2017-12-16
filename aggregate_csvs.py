7d16aaf4cea401b829e2d1335768ebaab0ac7a6e
Nicholas Dujay <nickdujay@gmail.com>
2017-12-16 13:53:23 -0500

    make aggregate csv work in any directory

--- aggregate_csvs.py
+++ aggregate_csvs.py
@@ -8,13 +8,14 @@ import datetime
 
 # complain about arguments
 parser = argparse.ArgumentParser(description='Aggregate CSVs for multiple accounts for easier hledger import.')
-parser.add_argument('--file', default='all', help='The name of the journal file to import')
+parser.add_argument('--import-directory', default='imports', help='The directory containing all the csvs')
+parser.add_argument('--output-file', default='all.csv', help='The aggregated csv output')
 args = parser.parse_args()
 
-# file is the first argument
-file = args.file
+IMPORT_DIRECTORY = os.path.abspath(args.import_directory)
+OUTPUT_FILE = os.path.abspath(args.output_file)
+OUTPUT_FILE_BASENAME = os.path.basename(OUTPUT_FILE)
 
-IMPORT_DIR = 'imports'
 # given a file name, return the account associated with it
 def get_account_name(import_file):
     accounts = import_file[:-len('.csv')].split('-')
@@ -52,32 +53,33 @@ def process_row(account_name, row):
     row[2] = account_name
     row[3] = row[3].strip()
     row[4] = row[4].strip()
-
-    # change GST to GST Credit
-    if contains(row[3], 'GST'):
-        row[3] = 'GST Credit'
-
     return row
 
-# get all the downloaded csv files
-import_files = [import_file for import_file in os.listdir(IMPORT_DIR) if not import_file.startswith(file) and import_file.endswith('.csv')]
+def main():
+    # get all the downloaded csv files
+    import_files = [
+      (os.path.join(IMPORT_DIRECTORY, import_file), get_account_name(import_file))
+      for import_file in os.listdir(IMPORT_DIRECTORY)
+      if not import_file.startswith(OUTPUT_FILE_BASENAME) and import_file.endswith('.csv')
+    ]
 
-# add all the csv files in one large array
-# it also does filter and map
-rows = []
-for import_file in import_files:
-    with open(IMPORT_DIR + '/' + import_file, newline='') as csv_file:
-        reader = csv.reader(csv_file, delimiter = ',')
-        account_name = get_account_name(import_file)
-        for row in reader:
-            if is_valid_row(account_name, row):
-                rows.append(process_row(account_name, row))
+    # add all the csv files in one large array
+    # it also does filter and map
+    rows = []
+    for (csv_file, account_name) in import_files:
+        with open(csv_file, newline='') as csv_file:
+            reader = csv.reader(csv_file, delimiter = ',')
+            for row in reader:
+                if is_valid_row(account_name, row):
+                    rows.append(process_row(account_name, row))
 
-# sort by date
-result = sorted(rows, key = lambda row: datetime.datetime.strptime(row[0], '%Y/%m/%d'))
+    # sort by date
+    result = sorted(rows, key = lambda row: datetime.datetime.strptime(row[0], '%Y/%m/%d'))
 
-# write the files out to the aggregated file
-out_file_name = IMPORT_DIR + '/' + file + '.csv'
-with open(out_file_name, 'w', newline='') as out_file:
-    writer = csv.writer(out_file, delimiter=',', quoting=csv.QUOTE_ALL)
-    writer.writerows(result)
+    # write the files out to the aggregated file
+    with open(OUTPUT_FILE, 'w', newline='') as out_file:
+        writer = csv.writer(out_file, delimiter=',', quoting=csv.QUOTE_ALL)
+        writer.writerows(result)
+
+if __name__ == '__main__':
+  main()
